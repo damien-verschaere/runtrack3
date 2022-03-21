2548138a50226808ab2b566e8f015fcd9b37022c
damien-verschaere <damien.verschaere@laplateforme.io>
2022-03-21 16:39:54 +0100

    job3 done

---
+++ Jour06/job03/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="drop">
+            <!-- menu drop vers les liens des pages  -->
+            <button class="dropbutton">MENU</button>
+            <div class="container-button">
+                <a href="">Accueil</a>
+                <a href="">Presentation</a>
+                <a href="">Contact</a>
+                
+            </div>
+        </div>
+    <div class="exo">
+    <section class="section">1</section>
+    <section class="section">2</section>
+    <section class="section">3</section>
+    <section class="section">4</section>
+    </div>
+</body>
+</html>
+<header>
+       
+    </header>
