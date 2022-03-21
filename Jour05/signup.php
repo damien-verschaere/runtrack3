e72569b9ace30f8c4bdad50fc851dbc767b8a02d
damien-verschaere <damien.verschaere@laplateforme.io>
2022-03-21 07:12:46 +0100

    j5 maj

---
+++ Jour05/signup.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+function  select(){
+    $bdd = new PDO("mysql:host = localhost ; dbname=utilisateurs", 'root', '');
+    $var =$bdd->prepare('SELECT * FROM utilisateurs');
+    $var->execute();
+    $result = $var->fetchall(PDO::FETCH_ASSOC);
+    $json = json_encode($result);
+}
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="index.js"></script>
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
