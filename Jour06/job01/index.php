51dbbcf9a8d43ab0ccef840412251b5eda6b020b
damien-verschaere <damien.verschaere@laplateforme.io>
2022-03-22 09:22:53 +0100

    maj exo

--- Jour06/job01/index.php
+++ Jour06/job01/index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style1.css">
-    <link rel="stylesheet" media="all and (min-device-width:1680px) and (max-device-width:1920px)" href="style2.css"/>
-    <link rel="stylesheet" media="all and (min-device-width:1280px) and (max-device-width:1679px)" href="style3.css"/>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" media="all and (min-device-width:1680px) and (max-device-width:1920px)" href="style2.css">
+    <link rel="stylesheet" media="all and (min-device-width:1280px) and (max-device-width:1680px)" href="style3.css">
+    <link rel="stylesheet" media="all and (max-device-width:1280px) " href="style4.css">
     <title>Document</title>
 </head>
 <body>
